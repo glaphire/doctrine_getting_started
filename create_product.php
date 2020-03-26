ee8bcab500e47e0d9a03304260fc09a74f079533
glaphire <motoryna.dariia@pdffiller.team>
2020-03-26 22:06:10 +0200

    added product insertion

---
+++ create_product.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once "bootstrap.php";
+
+$newProductName = $argv[1];
+
+$product = new Product();
+$product->setName($newProductName);
+
+$entityManager->persist($product);
+$entityManager->flush();
+
+echo "Created product with ID " . $product->getId() . PHP_EOL;
+
