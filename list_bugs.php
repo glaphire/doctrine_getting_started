2f0a1a45102ff34f305b7a0ea358ef398cb93247
glaphire <motoryna.dariia@pdffiller.team>
2020-03-30 20:28:11 +0300

    list bugs v2 (as arrays)

--- list_bugs.php
+++ list_bugs.php
@@ -2,18 +2,19 @@
 
 require_once "bootstrap.php";
 
-$dql = "SELECT b, e, r FROM Bug b JOIN b.engineer e JOIN b.reporter r ORDER BY b.created DESC";
+$dql = "SELECT b, e, r, p FROM Bug b JOIN b.engineer e "
+        . "JOIN b.reporter r JOIN b.products p ORDER BY b.created DESC";
 
 $query = $entityManager->createQuery($dql);
 $query->setMaxResults(30);
-$bugs = $query->getResult();
+$bugs = $query->getArrayResult();
 
 foreach ($bugs as $bug) {
-    echo $bug->getDescription() . " - " . $bug->getCreated()->format('d.m.Y') . PHP_EOL;
-    echo "  Reported by: " . $bug->getReporter()->getName() . PHP_EOL;
-    echo "  Assigned to: " . $bug->getEngineer()->getName() . PHP_EOL;
-    foreach ($bug->getProducts() as $product) {
-        echo "  Platform: " . $product->getName() . PHP_EOL;
+    echo $bug['description'] . " - " . $bug['created']->format('d.m.Y') . PHP_EOL;
+    echo "  Reported by: " . $bug['reporter']['name'] . PHP_EOL;
+    echo "  Assigned to: " . $bug['engineer']['name'] . PHP_EOL;
+    foreach ($bug['products'] as $product) {
+        echo "  Platform: " . $product['name'] . PHP_EOL;
     }
     echo PHP_EOL;
 }
